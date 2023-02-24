e580a7688b9b6d0c6c6a280f3a17e25db0ad60f6
Harry Mustoe-Playfair <harry.mustoeplayfair@gmail.com>
2023-02-24 22:03:41 +0000

    get rid of dist folder

--- dist/webpack.dev.config.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-var merge = require('webpack-merge').merge;
-var common = require('./webpack.config.js');
-module.exports = merge(common, {
-    devtool: 'inline-source-map'
-});
-//# sourceMappingURL=webpack.dev.config.js.map
--- dist/webpack.production.config.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-var merge = require('webpack-merge').merge;
-var common = require('./webpack.config.js');
-module.exports = merge(common, {
-    mode: 'production'
-});
-//# sourceMappingURL=webpack.production.config.js.map
